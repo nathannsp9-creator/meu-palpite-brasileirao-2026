857814e9f6220a877eaf23a3095d4a4d0fa98d08
Nathannael Silva <nathann.sp9@gmail.com>
2025-12-12 11:00:23 -0300

    fix(vite): force base path to root to fix MIME error

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/", // 🔴 ESSENCIAL
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
   },
 });
