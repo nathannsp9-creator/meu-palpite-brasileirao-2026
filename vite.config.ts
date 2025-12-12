f0669bf6f4c952554ec5fc1f83dcc6864027378f
Nathannael Silva <nathann.sp9@gmail.com>
2025-12-12 07:02:54 -0300

    fix: use plugin-react instead of swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
