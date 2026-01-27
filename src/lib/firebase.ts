9a37cb01b93743a8bf09c999843e0476f7f9b7bc
Miguel <mimiguelmota@hotmail.com>
2026-01-27 13:25:02 -0300

    Resolvendo conflito

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,17 @@
-
+// Firebase configuration and initialization
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// O código agora lê as variáveis do .env automaticamente
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAi3jvaNlijsLbtL3lRu9BaO88FN6zInu4",
+//   authDomain: "meu-palpite-testes.firebaseapp.com",
+//   projectId: "meu-palpite-testes",
+//   storageBucket: "meu-palpite-testes.firebasestorage.app",
+//   messagingSenderId: "889026394469",
+//   appId: "1:889026394469:web:85d9f76cca42e6e424498c"
+// };
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -13,13 +21,13 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
+// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 
-
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
 
 export default app;
