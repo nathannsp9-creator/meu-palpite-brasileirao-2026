48d92766dc8836ff1ca5155d8d2022a3aa9a0bea
Miguel <mimiguelmota@hotmail.com>
2026-01-26 22:00:23 -0300

    adicionando palpites da galera

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,22 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAi3jvaNlijsLbtL3lRu9BaO88FN6zInu4",
+//   authDomain: "meu-palpite-testes.firebaseapp.com",
+//   projectId: "meu-palpite-testes",
+//   storageBucket: "meu-palpite-testes.firebasestorage.app",
+//   messagingSenderId: "889026394469",
+//   appId: "1:889026394469:web:85d9f76cca42e6e424498c"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAi3jvaNlijsLbtL3lRu9BaO88FN6zInu4",
-  authDomain: "meu-palpite-testes.firebaseapp.com",
-  projectId: "meu-palpite-testes",
-  storageBucket: "meu-palpite-testes.firebasestorage.app",
-  messagingSenderId: "889026394469",
-  appId: "1:889026394469:web:85d9f76cca42e6e424498c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
